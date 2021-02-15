9d922feb9120408b94677239904728f0b9290982
El Capitán <heneverloses@gmail.com>
2021-02-14 19:37:17 -0600

    Vistas

---
+++ static/js/scripts.js
@@ -0,0 +1,34 @@
+// Main actions
+document.addEventListener("DOMContentLoaded", function() {
+    let body = document.querySelector("body");
+    let button_nav__nuevo = document.querySelector(".nav__nuevo");
+    let button_nav__consultas = document.querySelector(".nav__consultas");
+    let button_nav__configuraciones = document.querySelector(".nav__configuraciones");
+    let button_scan__options__url = document.querySelector(".scan__options__url");
+    let button_scan__options__file = document.querySelector(".scan__options__file");
+
+    button_nav__nuevo.addEventListener("click", function(){
+        body.classList.replace("vista_consultas", "vista_nuevo");
+        body.classList.replace("vista_configuraciones", "vista_nuevo");
+    });
+    
+    button_nav__consultas.addEventListener("click", function(){
+        body.classList.replace("vista_nuevo", "vista_consultas");
+        body.classList.replace("vista_configuraciones", "vista_consultas");
+    });
+    
+    button_nav__configuraciones.addEventListener("click", function(){
+        body.classList.replace("vista_nuevo", "vista_configuraciones");
+        body.classList.replace("vista_consultas", "vista_configuraciones");
+    });
+
+    button_scan__options__file.addEventListener("click", function(){
+        body.classList.replace("url", "file");
+    });
+
+    button_scan__options__file.addEventListener("click", function(){
+        body.classList.replace("url", "file");
+    });
+    
+});
+
