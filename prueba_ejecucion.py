25b65a4704489f80f2c54e68a6a2ebbbc63445ac
AndresCro <36737534+AndresCro@users.noreply.github.com>
2021-02-17 20:23:07 -0600

    ejemplo de json

--- prueba_ejecucion.py
+++ prueba_ejecucion.py
@@ -10,38 +10,16 @@ headers = {
 }
 
 peticion = {
-	"sitio":"http://altoromutual.com:8080",
+    "sitio":"http://altoromutual.com:8080",
     "ejecucion":"",
-	"dnsdumpster" : {
-		"revision":True,
-		"dns" : True,
-		"txt" : True,
-		"host" : True,
-		"mx" : True
-
-	},
-	"robtex" : {
-		"revision":True,
-		"informacion":True,
-		"dns_forward":True,
-		"mx_forward":True,
-		"host_forward":True,
-		"host_reverse":True
-	},
-	"puertos" : { 
-		"revision" : True,
-		"opcion" : "rango",
-		"rango" : {
-			"inicio" : 8070,
-			"final" : 8090
-		}
-	},
-    # Analisis
-    "algo":"",
-    #Fuzzing
+    "puertos" : { 
+	    "rango" : {
+		"inicio" : 8070,
+		"final" : 8090
+	    },
+    },
     "cookie":"PHDSESSID:jnj8mr8fugu61ma86p9o96frv0",
     "hilos":4,
-    #Explotacion
     "profundidad":2
 }
 
