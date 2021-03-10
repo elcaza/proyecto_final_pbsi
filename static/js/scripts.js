d02531b111cc2f50ff55f23147b5ba862387f938
El Capitán <heneverloses@gmail.com>
2021-03-10 01:29:05 -0600

    Recolocar items

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	let button_nav__nuevo = document.querySelector(".nav__nuevo");
 	let button_nav__consultas = document.querySelector(".nav__consultas");
 	let button_nav__proximosEscaneos = document.querySelector(".nav__proximosEscaneos");
+	let button_nav__exploits = document.querySelector(".nav__exploits");
 	let button_scan__options__url = document.querySelector(".scan__options__url");
 	let button_scan__options__file = document.querySelector(".scan__options__file");
 	let scan__start = document.querySelector(".scan__start");
@@ -20,17 +21,26 @@ document.addEventListener("DOMContentLoaded", function() {
 	button_nav__nuevo.addEventListener("click", function(){
 		body.classList.replace("vista_consultas", "vista_nuevo");
 		body.classList.replace("vista_configuraciones", "vista_nuevo");
+		body.classList.replace("vista_exploits", "vista_nuevo");
 	});
 	
 	button_nav__consultas.addEventListener("click", function(){
 		body.classList.replace("vista_nuevo", "vista_consultas");
 		body.classList.replace("vista_configuraciones", "vista_consultas");
+		body.classList.replace("vista_exploits", "vista_consultas");
 		start_consulta();
 	});
 	
 	button_nav__proximosEscaneos.addEventListener("click", function(){
 		body.classList.replace("vista_nuevo", "vista_configuraciones");
 		body.classList.replace("vista_consultas", "vista_configuraciones");
+		body.classList.replace("vista_exploits", "vista_configuraciones");
+	});
+
+	button_nav__exploits.addEventListener("click", function(){
+		body.classList.replace("vista_nuevo", "vista_exploits");
+		body.classList.replace("vista_consultas", "vista_exploits");
+		body.classList.replace("vista_configuraciones", "vista_exploits");
 	});
 
 	button_scan__options__url.addEventListener("click", function(){
