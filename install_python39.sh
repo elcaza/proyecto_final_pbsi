32b2d31f93cb750c1324a99c7cd284077c106d56
El Capitán <heneverloses@gmail.com>
2021-04-18 19:58:04 -0500

    install python 39

--- install_python39.sh
+++ install_python39.sh
@@ -1,4 +1,4 @@
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
+sudo apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libsqlite3-dev libreadline-dev libffi-dev curl libbz2-dev
 wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
 
 tar -xf Python-3.9.1.tgz
